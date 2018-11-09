3a32001f662aa7310856cee904871c2af7e1ee3f
mun9k1 <mv9k1@gmail.com>
2018-11-09 09:32:04 +0700

    second commit

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <div className="Hero">
+        <div className="HeroGroup">
+        <h1>Learn to design and code React apps</h1>
+        <p>Completed courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+        <p>Now go build something great - mungki.</p>
+       
+        <Link to="/video">Watch the video</Link>
+        </div>
+    </div>
+    
+  </div>
+)
+
+
+
+
+
+export default IndexPage
